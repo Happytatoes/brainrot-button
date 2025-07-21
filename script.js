9ce8ef26cc27e153b5e35acafe28b1804816c561
Noah Wong <noahwong@Noahs-MacBook-Air-2.local>
2025-07-20 19:16:46 -0500

    Add MongoDB

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var random; 
+var random;
 var audio;
 
-function playSound () {
-    random = Math.random();
-    if (random < 0.9) {
-        audio = document.getElementById("audio1"); 
-    }
-    else if (random < 0.99) {
-        audio = document.getElementById("audio2"); 
-    }
-    else {
-        audio = document.getElementById("audio3"); 
-    }
-    
-    audio.play(); 
-    audio.currentTime = 0;
+function playSound() {
+	random = Math.random();
+	if (random < 0.9) {
+		audio = document.getElementById("audio1");
+	}
+	else if (random < 0.99) {
+		audio = document.getElementById("audio2");
+	}
+	else {
+		audio = document.getElementById("audio3");
+	}
+
+	audio.play();
+	audio.currentTime = 0;
 }
 
 const textElement = document.getElementById("subtext");
@@ -22,28 +22,34 @@ const button = document.getElementById("brainrotbutton");
 
 // Update the text with the current count
 function updateClickText(count) {
-  textElement.textContent = `has been clicked ${count} times`;
+	textElement.textContent = `has been clicked ${count} times`;
 }
-
-// Get the current count when page loads
-fetch("https://brainrot-button-backend.onrender.com/count")
-  .then(res => res.json())
-  .then(data => updateClickText(data.count));
 
 // When the button is clicked, increment and update
 button.addEventListener("click", () => {
-    playSound();
-    fetch("https://brainrot-button-backend.onrender.com/increment", {
-    method: 'POST',
-  })
-    .then(res => res.json())
-    .then(data => updateClickText(data.count));
+	playSound();
+	fetch("https://brainrot-button-backend.onrender.com/increment", {
+		method: 'POST',
+	})
+		.then(res => res.json())
+		.then(data => updateClickText(data.count));
+});
+
+// Get the current count when page loads
+fetch("https://brainrot-button-backend.onrender.com/count")
+	.then(res => res.json())
+	.then(data => updateClickText(data.count));
+
+//incriment visitor count
+fetch("https://brainrot-button-backend.onrender.com/addvisitor", {
+	method: 'POST',
 });
 
 //fetch and display visit count
-fetch('https://your-render-backend-url.com/visit')
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("visit-count").textContent =
-      `This happytatoes.com site has been visited ${data.visitors} times`;
+fetch('https://brainrot-button-backend.onrender.com/visitors')
+	.then(res => res.json())
+	.then(data => {
+	document.getElementById("visit-count").textContent =
+		`This happytatoes.com site has been visited ${data.visitors} times`;
 });
+
